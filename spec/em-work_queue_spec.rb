fd81d5c97672611b36a956e9c8b206552871ec50
Terry Finn <terry@terryrfinn.com>
2012-04-15 20:59:10 -0400

    wrote spec for WorkQueue

--- spec/em-work_queue_spec.rb
+++ spec/em-work_queue_spec.rb
@@ -2,5 +2,33 @@ require 'spec_helper'
 include EM::WorkQueue
 
 describe WorkQueue do
-  
+  it 'should create a queue' do
+    WorkQueue.new.queue.class.should be_kind_of(EM::Queue.class)
+  end
+
+  it 'should push values onto the queue when push is called' do
+    EM.run do
+      subject = WorkQueue.new
+      subject.push 'foobar'
+      subject.queue.empty?.should == false
+      EM.stop
+    end
+  end
+
+  it 'should set running to false' do
+    WorkQueue.new.running?.should == false
+  end
+
+  it 'should set running to true when start is called' do
+    subject = WorkQueue.new
+    subject.start
+    subject.running?.should == true
+  end
+
+  it 'should set running to false when stop is called' do
+    subject = WorkQueue.new
+    subject.start
+    subject.stop
+    subject.running?.should == false
+  end
 end
